583b790bcd946cc170dfecfd96434b6d70ff57ed
E11I3N0R <byleree@gmail.com>
2022-05-05 01:03:02 -0500

    layout and theme changes

--- Byler_Nightingale Final Project Draft_ShinyApp.R
+++ Byler_Nightingale Final Project Draft_ShinyApp.R
@@ -20,107 +20,133 @@ Nightingale %>%
   mutate(Regime = factor(Regime, levels = c("Before", "After"))) %>%
   mutate(mo = month(Date, label = TRUE, abbr = TRUE)) -> long_data
 
+
 # APP UI
 ui <- fluidPage(
+  # colors include 78c2ad (mint), f3969a (pale brick), 6cc3d5 (pale blue)
+  # 6cc3d5 (bright mint), ffce67 (yellow), ff7851 (orange), 7C8083 (gray)
+  theme = bslib::bs_theme(bootswatch = "minty"),
+  
   tags$head(
     # Note the wrapping of the string in HTML()
     tags$style(HTML("
       @import url('https://fonts.googleapis.com/css2?family=Lato&family=Playball&family=Spectral:ital@1&display=swap');
       body {
-        font-family: 'Lato';
-        background-color: white;
-        color: black;
+        font-family: 'Lato'
       }
       h1 {
-        font-family: 'Playball';
+        font-family: 'Playball'
       }
       h3 {
-        font-family: 'Spectral', serif;
+        font-family: 'Spectral', serif
       }
       .shiny-input-container {
-        color: #474747;
       }"))
   ),
   
   titlePanel(h1("Florence Nightingale: Mother of Data Viz")),
   
-  sidebarLayout(position = "right",
-    sidebarPanel(tags$b("Sources:"),
+  navlistPanel(
+    
+    # INTRO SECTION
+    "Introduction",
+    
+    # ABOUT FLORENCE SECTION
+    tabPanel("About Florence Nightingale",
+      h3("About Florence Nightingale"),
+      p("This is some text explaining Florence Nightingale's work.")
+    ),
+    
+    # PLACEHOLDER SECTION
+    "Example Chart",
+    tabPanel("Slider Example",
+      h3("Slider Example"),
+      sliderInput(inputId = "num", label = "Choose a number", value = 25, min = 1, max = 100),
+      plotOutput("hist")
+    ),
+    
+    # ROSE DIAGRAM SECTION
+    "Rose Diagram",
+    
+    # ORIGINAL ROSE DIAGRAM
+    tabPanel("Original",
+       h3("Original Rose Diagram"),
+       img(src = "Rose_Diagram.jpg", height = 420, width = 768),
+       br(),
+       br(),
+       div(
+         p(tags$b("DIAGRAM of the CAUSES of MORTALITY in the ARMY in the EAST")),
+         p("Figure 1: APRIL 1854 to MARCH 1855"),
+         p("Figure 2: APRIL 1855 to MARCH 1856"),
+         tags$ul(
+           tags$li("The areas of the ",
+            span("blue", style = "color:SteelBlue", .noWS = "after"),
+            ", ",
+            span("red", style = "color:PaleVioletRed", .noWS = "after"),
+            ", & ",
+            span("black ", style = "color:LightSlateGray"),
+            "wedges are each measured from the center as the common vortex."),
+           tags$li("The ",
+             span("blue ", style = "color:SteelBlue"),
+             "wedges measured from the center of the circle represent area for area the deaths from ",
+             span("Preventable or Mitigable Zymotic diseases", style = "color:SteelBlue", .noWS = "after"),
+             ", the ",
+             span("red ", style = "color:PaleVioletRed"),
+             "wedges measured from the center the deaths from ",
+             span("wounds", style="color:PaleVioletRed", .noWS = "after"),
+             ", & the ",
+             span("black ", style = "color:LightSlateGray"),
+             "wedges measured from the center the deaths from ",
+             span("all other causes", style = "color:LightSlateGray", .noWS = "after"),
+             "."),
+           tags$li("The ",
+             span("black ", style = "color:LightSlateGray"),
+             "line across the ",
+             span("red ", style = "color:PaleVioletRed"),
+             "triangle in Nov. 1854 marks the boundary of the deaths from ",
+             span("all other causes ", style = "color:LightSlateGray"),
+             "during the month."),
+           tags$li("In October 1854 & April 1855, the ",
+             span("black ", style = "color:LightSlateGray"),
+             "area coincides with the ",
+             span("red", style = "color:PaleVioletRed", .noWS = "after"),
+             ", in January & February 1856, the ",
+             span("blue ", style = "color:SteelBlue"),
+             "coincides with the ",
+             span("black", style = "color:LightSlateGray", .noWS = "after"),
+             "."),
+           tags$li("The entire areas may be compared by following the ",
+             span("blue", style = "color:SteelBlue", .noWS = "after"),
+             ", the ",
+             span("red", style = "color:PaleVioletRed", .noWS = "after"),
+             ", & the ",
+             span("black ", style = "color:LightSlateGray"),
+             "lines enclosing them."
+           )
+         ),
+         em("—Florence Nightingale, Contribution to the Sanitary History of the British Army (1859)")
+       ),
+    
+    ),
+    
+    # RECREATION ROSE DIAGRAM
+    tabPanel("Recreation",
+      h3("Modern Recreation with R"),
+      plotOutput("rose")
+    ),
+    
+    "-----",
+    
+    # SOURCES TAB
+    tabPanel("Sources",
+      h3("Sources:"),
       tags$ul(
         tags$li(tags$a(href="https://github.com/vincentarelbundock/Rdatasets/blob/master/csv/HistData/Nightingale.csv", "Nightingale Dataset")),
         tags$li(tags$a(href="http://understandinguncertainty.org/node/214", "Mathematics of the Coxcombs")),
         tags$li(tags$a(href="https://rpubs.com/chidungkt/819554", "Florence Nightingale’s Rose Diagram"))
       )
-    ),
-    
-    mainPanel(h3("main panel"),
-      sliderInput(inputId = "num", label = "Choose a number", value = 25, min = 1, max = 100),
-      plotOutput("hist"),
-      br(),
-      hr(),
-      br(),
-      h3("Original Rose Diagram"),
-      img(src = "Rose_Diagram.jpg", height = 350, width = 640),
-      div(
-        tags$blockquote(
-          p("DIAGRAM of the CAUSES of MORTALITY in the ARMY in the EAST"),
-          p("Figure 1: APRIL 1854 to MARCH 1855"),  
-          p("Figure 2: APRIL 1855 to MARCH 1856"),
-          tags$ul(
-            tags$li("The areas of the ",
-              span("blue", style = "color:blue", .noWS = "after"),
-              ", ",
-              span("red", style = "color:red", .noWS = "after"),
-              ", & ",
-              span("black ", style = "color:gray"),
-              "wedges are each measured from the center as the common vortex."),
-            tags$li("The ",
-              span("blue ", style = "color:blue"),
-              "wedges measured from the center of the circle represent area for area the deaths from ",
-              span("Preventable or Mitigable Zymotic diseases", style = "color:blue", .noWS = "after"),
-              ", the ",
-              span("red ", style = "color:red"),
-              "wedges measured from the center the deaths from ",
-              span("wounds", style="color:red", .noWS = "after"),
-              ", & the ",
-              span("black ", style = "color:gray"),
-              "wedges measured from the center the deaths from ",
-              span("all other causes", style = "color:gray", .noWS = "after"),
-              "."),
-            tags$li("The ",
-              span("black ", style = "color:gray"),
-              "line across the ",
-              span("red ", style = "color:red"),
-              "triangle in Nov. 1854 marks the boundary of the deaths from ",
-              span("all other causes ", style = "color:gray"),
-              "during the month."),
-            tags$li("In October 1854 & April 1855, the ",
-              span("black ", style = "color:gray"),
-              "area coincides with the ",
-              span("red", style = "color:red", .noWS = "after"),
-              ", in January & February 1856, the ",
-              span("blue ", style = "color:blue"),
-              "coincides with the ",
-              span("black", style = "color:gray", .noWS = "after"),
-              "."),
-            tags$li("The entire areas may be compared by following the ",
-              span("blue", style = "color:blue", .noWS = "after"),
-              ", the ",
-              span("red", style = "color:red", .noWS = "after"),
-              ", & the ",
-              span("black ", style = "color:gray"),
-              "lines enclosing them."
-              )
-            ),
-          br(),
-          em("—Florence Nightingale")
-        )
-      ),
-      br(),
-      plotOutput("rose")
-      )
-    ),
-
+    )
+  ),
   
 
 )
@@ -138,16 +164,16 @@ server <- function(input, output) {
   output$rose <- renderPlot({
     title <- "Rose Diagram"
     long_data %>% 
-      ggplot(aes(x = mo, y = Deaths, fill = Cause))+
+      ggplot(aes(x = mo, y = Deaths, fill = Cause)) +
       geom_col(color = "grey20") + 
-      scale_fill_manual(values = c("firebrick", "orange", "#365181"), name = "") +
+      scale_fill_manual(values = c("#6cc3d5", "#7C8083", "#f3969a"), name = "") +
       scale_y_sqrt() +
       facet_wrap(~ Regime) + 
-      coord_equal(ratio = 1) +  
+      coord_equal(ratio = 1) +
       coord_polar() +
       labs(title = "Causes of Mortality in the Army in the East", 
-           subtitle = "Created by R Statistical Software", 
-           caption = "Data Source: Deaths from various causes in the Crimean War") + 
+           subtitle = "BEFORE and AFTER March 1855",
+           caption = "Data Source: Deaths from various causes in the Crimean War") +
       theme(legend.position = "top") + 
       theme(text = element_text(size = 14)) + 
       theme(axis.title.y = element_blank()) + 
@@ -155,12 +181,14 @@ server <- function(input, output) {
       theme(axis.text.y = element_blank()) + 
       theme(axis.ticks = element_blank()) + 
       theme(plot.margin = unit(rep(0.7, 4), "cm")) + 
-      theme(plot.title = element_text(color = "black", size = 20)) + 
+      theme(plot.title = element_text(color = "#78c2ad", size = 20, face="bold")) + 
       theme(plot.caption = element_text(color = "grey70", size = 11)) + 
-      theme(plot.subtitle = element_text(color = "grey70", size = 13)) +
+      theme(plot.subtitle = element_text(color = "#7C8083", size = 13, face="bold")) +
       theme(legend.text = element_text(color = "black", size = 12)) + 
       theme(strip.text = element_text(color = "black", size = 14, face = "bold", hjust = 0.5))
-  })
+  },
+  width = 682,
+  height = 528)
 }
 
 shinyApp(ui = ui, server = server)
