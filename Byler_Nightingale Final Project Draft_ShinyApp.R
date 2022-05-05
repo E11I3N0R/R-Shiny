b5313ee07d9e84c83f5be2934bdca4f1715003be
E11I3N0R <byleree@gmail.com>
2022-05-04 21:41:30 -0500

    more changes to draft

--- Byler_Nightingale Final Project Draft_ShinyApp.R
+++ Byler_Nightingale Final Project Draft_ShinyApp.R
@@ -22,9 +22,107 @@ Nightingale %>%
 
 # APP UI
 ui <- fluidPage(
-  sliderInput(inputId = "num", label = "Choose a number", value = 25, min = 1, max = 100),
-  plotOutput("hist"),
-  plotOutput("rose")
+  tags$head(
+    # Note the wrapping of the string in HTML()
+    tags$style(HTML("
+      @import url('https://fonts.googleapis.com/css2?family=Lato&family=Playball&family=Spectral:ital@1&display=swap');
+      body {
+        font-family: 'Lato';
+        background-color: white;
+        color: black;
+      }
+      h1 {
+        font-family: 'Playball';
+      }
+      h3 {
+        font-family: 'Spectral', serif;
+      }
+      .shiny-input-container {
+        color: #474747;
+      }"))
+  ),
+  
+  titlePanel(h1("Florence Nightingale: Mother of Data Viz")),
+  
+  sidebarLayout(position = "right",
+    sidebarPanel(tags$b("Sources:"),
+      tags$ul(
+        tags$li(tags$a(href="https://github.com/vincentarelbundock/Rdatasets/blob/master/csv/HistData/Nightingale.csv", "Nightingale Dataset")),
+        tags$li(tags$a(href="http://understandinguncertainty.org/node/214", "Mathematics of the Coxcombs")),
+        tags$li(tags$a(href="https://rpubs.com/chidungkt/819554", "Florence Nightingale’s Rose Diagram"))
+      )
+    ),
+    
+    mainPanel(h3("main panel"),
+      sliderInput(inputId = "num", label = "Choose a number", value = 25, min = 1, max = 100),
+      plotOutput("hist"),
+      br(),
+      hr(),
+      br(),
+      h3("Original Rose Diagram"),
+      img(src = "Rose_Diagram.jpg", height = 350, width = 640),
+      div(
+        tags$blockquote(
+          p("DIAGRAM of the CAUSES of MORTALITY in the ARMY in the EAST"),
+          p("Figure 1: APRIL 1854 to MARCH 1855"),  
+          p("Figure 2: APRIL 1855 to MARCH 1856"),
+          tags$ul(
+            tags$li("The areas of the ",
+              span("blue", style = "color:blue", .noWS = "after"),
+              ", ",
+              span("red", style = "color:red", .noWS = "after"),
+              ", & ",
+              span("black ", style = "color:gray"),
+              "wedges are each measured from the center as the common vortex."),
+            tags$li("The ",
+              span("blue ", style = "color:blue"),
+              "wedges measured from the center of the circle represent area for area the deaths from ",
+              span("Preventable or Mitigable Zymotic diseases", style = "color:blue", .noWS = "after"),
+              ", the ",
+              span("red ", style = "color:red"),
+              "wedges measured from the center the deaths from ",
+              span("wounds", style="color:red", .noWS = "after"),
+              ", & the ",
+              span("black ", style = "color:gray"),
+              "wedges measured from the center the deaths from ",
+              span("all other causes", style = "color:gray", .noWS = "after"),
+              "."),
+            tags$li("The ",
+              span("black ", style = "color:gray"),
+              "line across the ",
+              span("red ", style = "color:red"),
+              "triangle in Nov. 1854 marks the boundary of the deaths from ",
+              span("all other causes ", style = "color:gray"),
+              "during the month."),
+            tags$li("In October 1854 & April 1855, the ",
+              span("black ", style = "color:gray"),
+              "area coincides with the ",
+              span("red", style = "color:red", .noWS = "after"),
+              ", in January & February 1856, the ",
+              span("blue ", style = "color:blue"),
+              "coincides with the ",
+              span("black", style = "color:gray", .noWS = "after"),
+              "."),
+            tags$li("The entire areas may be compared by following the ",
+              span("blue", style = "color:blue", .noWS = "after"),
+              ", the ",
+              span("red", style = "color:red", .noWS = "after"),
+              ", & the ",
+              span("black ", style = "color:gray"),
+              "lines enclosing them."
+              )
+            ),
+          br(),
+          em("—Florence Nightingale")
+        )
+      ),
+      br(),
+      plotOutput("rose")
+      )
+    ),
+
+  
+
 )
 
 # APP SERVER
